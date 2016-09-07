b115f815f5ecb47729fd2525a5afac86b1953006
Ju Zhang <jzha263@aucklanduni.ac.nz>
2016-09-07 21:41:58 +1200

    added requirements.txt, bumped to v0.1.1

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 from setuptools import setup, find_packages
-import sys, os
+import sys, os, io
+
+def readfile(filename, split=False):
+    with io.open(filename, encoding="utf-8") as stream:
+        if split:
+            return stream.read().split("\n")
+        return stream.read()
+
+readme = readfile("README.md", split=True)[3:]  # skip title
+requires = readfile("requirements.txt", split=True)
+license = readfile("LICENSE")
 
 setup(name=u'mapclientplugins.fieldworkgait2392geomstep',
-      version='0.1.0',
-      description='',
-      long_description="",
-      classifiers=[],
-      author=u'Ju Zhang',
-      author_email='',
+    version='0.1.1',
+    description='',
+    long_description=''.join(readme) + license,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+    ],
+    author=u'Ju Zhang',
+    author_email='',
       url='https://github.com/mapclient-plugins/fieldworkgait2392geomstep',
-      license='GPL',
+      license='APACHE',
       packages=find_packages(exclude=['ez_setup',]),
       namespace_packages=['mapclientplugins'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'numpy',
-          'scipy',
-          'transforms3d',
-      ],
+      install_requires=requires,
       )
