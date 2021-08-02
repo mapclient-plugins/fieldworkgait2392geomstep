2b115c75c06a3911d603d84a192008504fd4e26d
TimmyNic <tim.nicolas@outlook.com>
2021-08-02 13:30:14 +1200

    Updated configure step to accept relative paths

--- mapclientplugins/fieldworkgait2392geomstep/step.py
+++ mapclientplugins/fieldworkgait2392geomstep/step.py
@@ -151,6 +151,7 @@ class FieldworkGait2392GeomStep(WorkflowStepMountPoint):
             self._configured = True
         """
         dlg = ConfigureDialog(self._main_window)
+        dlg.setWorkflowLocation(self._location)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
@@ -190,6 +191,7 @@ class FieldworkGait2392GeomStep(WorkflowStepMountPoint):
         self._config.update(json.loads(string))
 
         d = ConfigureDialog(self._main_window)
+        d.setWorkflowLocation(self._location)
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
