b9506252e24f8eb4ef1754cbacb1b87adfaf3581
TimmyNic <tim.nicolas@outlook.com>
2021-08-31 20:34:22 +1200

    Updated the way dependencies are checked.
    Needed to update the way the dependencies of the plugin are defined to be consistent with the latest MAP Client framework update.

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ This file is part of MAP Client. (http://launchpad.net/mapclient)
 """
 
 from setuptools import setup, find_packages
-import sys, os, io
+import io
+
 
 def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
@@ -27,33 +28,41 @@ def readfile(filename, split=False):
             return stream.read().split("\n")
         return stream.read()
 
-readme = readfile("README.md", split=True)[3:]  # skip title
-requires = readfile("requirements.txt", split=True)
-license = readfile("LICENSE")
+
+package_readme = readfile("README.md", split=True)[3:]  # skip title
+package_license = readfile("LICENSE")
+# Need to update opensim reference to include install link.
+package_dependencies = [
+    "transforms3d",
+    "setuptools",
+    "PySide2",
+    "numpy",
+    "gias2",
+    "opensim @ ???"
+]
 package_data = {
-  'mapclientplugins.fieldworkgait2392geomstep': [
-    'data/*',
-  ],
+    'mapclientplugins.fieldworkgait2392geomstep': [
+        'data/*',
+    ],
 }
 
-
 setup(name=u'mapclientplugins.fieldworkgait2392geomstep',
-    version='0.1.3',
-    description='',
-    long_description='\n'.join(readme) + license,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-    ],
-    author=u'Ju Zhang',
-    author_email='',
-    url='https://github.com/mapclient-plugins/fieldworkgait2392geomstep',
-    license='APACHE',
-    packages=find_packages(exclude=['ez_setup',]),
-    namespace_packages=['mapclientplugins'],
-    include_package_data=True,
-    package_data=package_data,
-    zip_safe=False,
-    install_requires=requires,
-    )
+      version='0.1.3',
+      description='',
+      long_description='\n'.join(package_readme) + package_license,
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+      ],
+      author=u'Ju Zhang',
+      author_email='',
+      url='https://github.com/mapclient-plugins/fieldworkgait2392geomstep',
+      license='APACHE',
+      packages=find_packages(exclude=['ez_setup', ]),
+      namespace_packages=['mapclientplugins'],
+      include_package_data=True,
+      package_data=package_data,
+      zip_safe=False,
+      install_requires=package_dependencies,
+      )
