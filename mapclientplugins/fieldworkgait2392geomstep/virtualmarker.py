e054a23de9a5eedaff2db4a66254eddc6e213d91
TimmyNic <tim.nicolas@outlook.com>
2021-09-02 14:19:04 +1200

    Complete overhaul of the model scaling method.
    A number of changes have been made to the way the plugin scales the OpenSim Model. Many of these changes were necessary for the final OpenSim::Model to be compatible with OpenSim 4.2, the rest aim to simplify and optimize the existing code.

--- mapclientplugins/fieldworkgait2392geomstep/virtualmarker.py
+++ mapclientplugins/fieldworkgait2392geomstep/virtualmarker.py
@@ -1,5 +1,5 @@
 """
-Module for reading Gait2392's virtual markerset
+Module for reading Gait2392's virtual marker set.
 """
 
 import os
@@ -9,15 +9,11 @@ import numpy as np
 from gias2.musculoskeletal.osim import Marker
 
 SELF_DIR = os.path.split(os.path.realpath(__file__))[0]
-MARKERSET_PATH = str(os.path.join(SELF_DIR, 'data', 'gait2392_Scale_MarkerSet.xml'))
+MARKERSET_PATH = str(os.path.join(SELF_DIR, 'data',
+                                  'gait2392_Scale_MarkerSet.xml'))
 MARKER_OFFSET_PATH = str(os.path.join(SELF_DIR, 'data/', 'marker_offsets.dat'))
-try:
-    opensim_version = getattr(opensim, '__version__')
-except AttributeError:
-    opensim_version = None
 
-# dictionary mapping fieldwork landmark names to gait2392's virtual marker
-# names
+# Dictionary mapping fieldwork landmark names to gait2392 virtual marker names
 marker_name_map = {
     'pelvis-RASIS': 'R.ASIS',
     'pelvis-LASIS': 'L.ASIS',
@@ -40,58 +36,43 @@ marker_name_map = {
 
 
 def _load_virtual_markers():
-    markers = {}
-    marker_coords = {}
+    virtual_markers = {}
+    virtual_marker_coords = {}
 
-    if opensim_version == 4.0:
-        _dummy_model = opensim.Model()
-        _osim_markerset = opensim.MarkerSet(_dummy_model, MARKERSET_PATH)
-    else:
-        _osim_markerset = opensim.MarkerSet(MARKERSET_PATH)
-
+    _osim_markerset = opensim.MarkerSet(MARKERSET_PATH)
     for mi in range(_osim_markerset.getSize()):
         osim_marker = _osim_markerset.get(mi)
+        marker = Marker(osim_marker.clone())
+        virtual_markers[marker.name] = marker
+        virtual_marker_coords[marker.name] = marker.location
 
-        # create a copy because the markerset and its marers only exists
-        # in the function
-        _v = opensim.Vec3()
-        osim_marker.getOffset(_v)
-        offset = np.array([_v.get(i) for i in range(3)])
-        marker = Marker(
-            name=osim_marker.getName(),
-            bodyname=osim_marker.getBodyName(),
-            offset=offset,
-        )
-        markers[marker.name] = marker
-        marker_coords[marker.name] = marker.offset
-
-    return markers, marker_coords
+    return virtual_markers, virtual_marker_coords
 
 
-def _add_synthetic_markers(marker, marker_coords):
-    # add hip joint centres to markers (joint coords taken from gait2392)
-    markers['L.HJC'] = Marker(
-        bodyname='pelvis',
-        offset=(-0.0707, -0.0661, -0.0835)
-    )
-    marker_coords['L.HJC'] = markers['L.HJC'].offset
-    markers['R.HJC'] = Marker(
-        bodyname='pelvis',
-        offset=(-0.0707, -0.0661, 0.0835)
-    )
-    marker_coords['R.HJC'] = markers['R.HJC'].offset
+def _add_synthetic_markers(markers_in, marker_coords_in):
+    # Add hip joint centres to markers (joint coords taken from gait2392).
+    markers_in['L.HJC'] = Marker(name='L.HJC', frame_name='pelvis',
+                                 location=(-0.0707, -0.0661, -0.0835))
+    marker_coords_in['L.HJC'] = markers_in['L.HJC'].location
+    markers_in['R.HJC'] = Marker(name='R.HJC', frame_name='pelvis',
+                                 location=(-0.0707, -0.0661, 0.0835))
+    marker_coords_in['R.HJC'] = markers_in['R.HJC'].location
 
-    # add femur head centre to markers
-    markers['L.FHC'] = Marker(bodyname='femur_l', offset=(0, 0, 0))
-    marker_coords['L.FHC'] = markers['L.FHC'].offset
-    markers['R.FHC'] = Marker(bodyname='femur_r', offset=(0, 0, 0))
-    marker_coords['R.FHC'] = markers['R.FHC'].offset
+    # Add femur head centre to markers.
+    markers_in['L.FHC'] = Marker(name='L.FHC', frame_name='femur_l',
+                                 location=(0, 0, 0))
+    marker_coords_in['L.FHC'] = markers_in['L.FHC'].location
+    markers_in['R.FHC'] = Marker(name='R.FHC', frame_name='femur_r',
+                                 location=(0, 0, 0))
+    marker_coords_in['R.FHC'] = markers_in['R.FHC'].location
 
-    # add knee centre in tibia frame to markers
-    markers['L.Tib.KJC'] = Marker(bodyname='tibia_l', offset=(0, 0, 0))
-    marker_coords['L.Tib.KJC'] = markers['L.Tib.KJC'].offset
-    markers['R.Tib.KJC'] = Marker(bodyname='tibia_r', offset=(0, 0, 0))
-    marker_coords['R.Tib.KJC'] = markers['R.Tib.KJC'].offset
+    # Add knee centre in tibia frame to markers.
+    markers_in['L.Tib.KJC'] = Marker(name='L.Tib.KJC', frame_name='tibia_l',
+                                     location=(0, 0, 0))
+    marker_coords_in['L.Tib.KJC'] = markers_in['L.Tib.KJC'].location
+    markers_in['R.Tib.KJC'] = Marker(name='R.Tib.KJC', frame_name='tibia_r',
+                                     location=(0, 0, 0))
+    marker_coords_in['R.Tib.KJC'] = markers_in['R.Tib.KJC'].location
 
 
 def _load_marker_offsets():
@@ -99,33 +80,35 @@ def _load_marker_offsets():
     Read from file the offsets between the opensim virtual markers and 
     their corresponding fieldwork landmarks
     """
-    marker_offsets = {}
+    marker_offsets_list = {}
     with open(MARKER_OFFSET_PATH, 'r') as f:
         lines = f.readlines()
 
-    for l in lines:
-        if l[0] == '#':
+    for line in lines:
+        if line[0] == '#':
             pass
         else:
-            words = l.split()
+            words = line.split()
             if len(words) == 4:
-                marker_offsets[words[0]] = np.array([float(x) for x in words[1:]])
+                marker_offsets_list[words[0]] = np.array(
+                    [float(x) for x in words[1:]])
 
-    return marker_offsets
+    return marker_offsets_list
 
 
-# load up virtual markers from file
+# Load the virtual markers from file.
 markers, marker_coords = _load_virtual_markers()
 marker_offsets = _load_marker_offsets()
+g2392_markers = markers.copy()
 
-# add some additional "markers" based on anatomical/functional landmarks
+# Add some additional "markers" based on anatomical/functional landmarks.
 _add_synthetic_markers(markers, marker_coords)
 
 
 def get_equiv_vmarker_coords(fw_name):
     """
-    Return the coordinates of the gait2392 virtual marker coordinate
-    equivalent to the fieldwork landmark name
+    Return the coordinates of the gait2392 virtual marker coordinate equivalent
+    to the fieldwork landmark name
     """
     g2392_name = marker_name_map[fw_name]
     return marker_coords[g2392_name]
