c7b66328134bd5dd3cd339e6b58092c6457c5570
TimmyNic <tim.nicolas@outlook.com>
2022-12-19 17:46:09 +1300

    Set version to 0.3.0

--- mapclientplugins/fieldworkgait2392geomstep/__init__.py
+++ mapclientplugins/fieldworkgait2392geomstep/__init__.py
@@ -1,7 +1,7 @@
 """
 MAP Client Plugin
 """
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Ju Zhang'
 __stepname__ = 'Fieldwork Gait2392 Geometry'
 __location__ = 'https://github.com/mapclient-plugins/fieldworkgait2392geomstep/archive/master.zip'
